92fecbdbfe78fd80cc772de118a951687918480f
rastemoh <rastemoh@gmail.com>
2017-05-11 10:49:29 +0430

    preparing main page

--- modules/pages/client/pages.client.module.js
+++ modules/pages/client/pages.client.module.js
@@ -2,9 +2,9 @@
   'use strict';
 
   app.registerModule('pages', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('pages.custom', ['pages', 'slider.services']);// The core module is required for special route handling; see /core/client/config/core.client.routes
   app.registerModule('pages.admin', ['core.admin', 'ngCkeditor']);
   app.registerModule('pages.admin.routes', ['core.admin.routes']);
   app.registerModule('pages.services');
   app.registerModule('pages.routes', ['ui.router', 'core.routes', 'pages.services']);
+  app.registerModule('pages.custom', ['pages', 'slider.services', 'agPerson.services']);// The core module is required for special route handling; see /core/client/config/core.client.routes
 }(ApplicationConfiguration));
